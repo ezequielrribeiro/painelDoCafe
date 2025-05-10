88647758bb13f1f11df57c9351f9c0642f041122
Ezequiel R. Ribeiro <ezeqroot@gmail.com>
2025-05-10 01:12:24 -0300

    Refactor LoadPromos class to store products in a dictionary and convert to markdown format
    Updated the LoadPromos class to use a dictionary for storing product information, categorized by manufacturer. Introduced a new method to convert the product data into markdown format, enhancing the output structure. Adjusted the scraping logic to initialize the product list for "Encantos do Café" and improved the overall data handling.

--- src/scraping/load_promo.py
+++ src/scraping/load_promo.py
@@ -3,22 +3,37 @@ from selenium.webdriver.common.by import By
 
 class LoadPromos:
 
-    __products: list[dict]
+    __products: dict[str, list[dict]]
 
     def __init__(self):
-        self.__products = []
+        self.__products = {}
         self.__scraper = SeleniumScraper()
-        self.__markdown = "# Promoções\n\n"
+        self.__markdown = ''
 
     def scrapePromos(self) -> None:
         self._scrapePromoEncantos()
         self.__scraper.scrapeEnd()
+        self.__convertPromoToMarkdown()
+
+    def __convertPromoToMarkdown(self) -> None:
+        # Convert products dictionary to markdown format
+        for product_manufacturer, product_list in self.__products.items():
+            self.__markdown = f"## {product_manufacturer}\n\n"
+            for product in product_list:
+                self.__markdown += f"### {product['name']}\n\n"
+                self.__markdown += f"- preço: ~~{product['original_price']}~~ {product['discount_price']}\n\n\n\n"
+                self.__markdown += f"{product['description']}\n\n\n\n"
+                self.__markdown += f"- URL: [{product['link']}]({product['link']})\n\n"
+                self.__markdown += "---\n\n"
 
     def getPromosMarkdown(self) -> str:
         return self.__markdown
 
     def _scrapePromoEncantos(self) -> None:
         body = self.__scraper.scrapeSite("https://loja.encantosdocafe.com.br/produtos?promotion=1&page=1")
+        # Initialize the list for Encantos do Café products
+        self.__products['Encantos do Café'] = []
+        
         # get product names and prizes
         for a in body.find_elements(By.CLASS_NAME, 'product-link'):
             # ignore elements with blank prices and discounts
@@ -32,22 +47,12 @@ class LoadPromos:
                 'link': a.get_attribute('href')
             }
             # Add to products list
-            self.__products.append(product_info)
+            self.__products['Encantos do Café'].append(product_info)
         
         # get product description
-        for product in self.__products:
+        for product in self.__products['Encantos do Café']:
             body_a = self.__scraper.scrapeSite(product['link'])
             product['description'] = body_a.find_element(By.CLASS_NAME, 'product-description-item').text
-
-        # Convert products list to markdown format
-        self.__markdown = "## Encantos do Café\n\n"
-        for product in self.__products:
-            self.__markdown += f"- produto: {product['name']}\n\n"
-            self.__markdown += f"- descrição: {product['description']}\n\n"
-            self.__markdown += f"- preço original: {product['original_price']}\n\n"
-            self.__markdown += f"- preço com desconto: {product['discount_price']}\n\n"
-            self.__markdown += f"- URL: [{product['link']}]({product['link']})\n\n"
-            self.__markdown += "---\n\n"
 
 def main():
     promo = LoadPromos()
