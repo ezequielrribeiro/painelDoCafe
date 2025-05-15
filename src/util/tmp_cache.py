f5be7dc192924f8115855155a5726d9539d8f3bb
Ezequiel R. Ribeiro <ezeqroot@gmail.com>
2025-05-14 22:52:46 -0300

    Enhance TmpCache class with item expiration and storage
    Updated the TmpCache class to include a private dictionary for storing cache items, allowing for the addition of items with configurable expiration times.

--- src/util/tmp_cache.py
+++ src/util/tmp_cache.py
@@ -1,9 +1,10 @@
-
 class TmpCache:
+    __cache_items: dict[str, list[dict]]
 
     def __init__(self, file_path: str = "tmp_cache.json", default_expiration_time: int = 24) -> None:
         self.set_file_path(file_path)
         self.set_default_expiration_time(default_expiration_time)
+        self.__cache_items = {}
 
     def load(self) -> None:
         pass
@@ -11,14 +12,18 @@ class TmpCache:
     def flush(self) -> None:
         pass
 
-    def add_item(self, item, key: str) -> None:
-        pass
+    def add_item(self, item, key: str, expiration_time: int = None) -> None:
+        if expiration_time == None:
+            expiration_time = self.__default_expiration_time
+
+        cache_item = {'data' : item, 'expiration_time' : expiration_time}
+        self.__cache_items[key] = cache_item
 
     def get_item(self, key: str):
-        pass
+        return self.__cache_items[key]['data']
 
     def get_all_items(self):
-        pass
+        return {key: item['data'] for key, item in self.__cache_items.items()}
 
     def set_default_expiration_time(self, default_expiration_time: int = 24) -> None:
         self.__default_expiration_time = default_expiration_time
